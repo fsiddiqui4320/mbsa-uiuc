a613f9aa49be27d1053c9f4b095edeafff6d2055
Big Claw <bigclaw@Bigs-MacBook-Pro.local>
2026-05-24 14:56:58 -0500

    Fix font rendering: remove unsupported OpenType features, add preconnects for faster font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <head>
+        <link rel="preconnect" href="https://api.fontshare.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://cdn.fontshare.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,700&display=swap"
           rel="stylesheet"
